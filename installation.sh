ea89f8334864a52442d9563098147fe0db1324ac
shivansh <dHoneysh@gmail.com>
2025-01-08 21:12:56 +0530

    fixed the bug in installation script

--- installation.sh
+++ installation.sh
@@ -1,6 +1,8 @@
-current_directory=$(dirname "$0")
+cd $(dirname "$0")
+current_directory="$(pwd)"
 if [ ! -d "$HOME/.emacs.d" ]; then
     mkdir "$HOME/.emacs.d"
 fi
-ln -s "${current_directory%??}/init.el" "$HOME/.emacs.d/init.el"
-ln -s "${current_directory%??}/themes" "$HOME/.emacs.d/themes"
+
+ln -s "${current_directory}/init.el" "$HOME/.emacs.d/init.el"
+ln -s "${current_directory}/themes" "$HOME/.emacs.d/themes"
